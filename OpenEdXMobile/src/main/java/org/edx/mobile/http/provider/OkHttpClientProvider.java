873e4c7683be53c88c5b260ef6ef8e7c552ff4fd
laibinzhi <444239383@qq.com>
2019-08-20 15:21:21 +0800

    修改okhttp读取连接超时间

--- OpenEdXMobile/src/main/java/org/edx/mobile/http/provider/OkHttpClientProvider.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/http/provider/OkHttpClientProvider.java
@@ -22,16 +22,24 @@ import org.edx.mobile.http.util.Tls12SocketFactory;
 
 import java.io.File;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
+import okhttp3.ConnectionPool;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 public interface OkHttpClientProvider extends Provider<OkHttpClient> {
-    @NonNull OkHttpClient get();
-    @NonNull OkHttpClient getWithOfflineCache();
-    @NonNull OkHttpClient getNonOAuthBased();
+    @NonNull
+    OkHttpClient get();
+
+    @NonNull
+    OkHttpClient getWithOfflineCache();
+
+    @NonNull
+    OkHttpClient getNonOAuthBased();
+
 
     @Singleton
     class Impl implements OkHttpClientProvider {
@@ -39,6 +47,12 @@ public interface OkHttpClientProvider extends Provider<OkHttpClient> {
 
         private static final int FLAG_IS_OAUTH_BASED = 1;
         private static final int USES_OFFLINE_CACHE = 1 << 1;
+
+        final static int READ_TIMEOUT = 100;
+
+        final static int CONNECT_TIMEOUT = 60;
+
+        final static int WRITE_TIMEOUT = 60;
 
         @Inject
         private Context context;
@@ -70,6 +84,15 @@ public interface OkHttpClientProvider extends Provider<OkHttpClient> {
             OkHttpClient client = clients[index];
             if (client == null) {
                 final OkHttpClient.Builder builder = new OkHttpClient.Builder();
+                //读取超时
+                builder.readTimeout(READ_TIMEOUT, TimeUnit.SECONDS);
+                //连接超时
+                builder.connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS);
+                //写入超时
+                builder.writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS);
+                //自定义连接池最大空闲连接数和等待时间大小，否则默认最大5个空闲连接
+                builder.connectionPool(new ConnectionPool(32, 5, TimeUnit.MINUTES));
+
                 List<Interceptor> interceptors = builder.interceptors();
                 if (usesOfflineCache) {
                     final File cacheDirectory = new File(context.getFilesDir(), "http-cache");
